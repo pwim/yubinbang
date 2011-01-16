c2339572eaf2c0a500a798d14e4a92623f8ba328
Paul McMahon <paul@mobalean.com>
2011-01-16 13:32:17 +0900

    Add convenience method

--- lib/yubinbang/raw_data.rb
+++ lib/yubinbang/raw_data.rb
@@ -4,6 +4,15 @@ require 'fastercsv'
 
 module Yubinbang
   module RawData
+    # Fetch data from Japan Post site and parse it to an array of hashes
+    def self.parse_from_source(tmp_path)
+      raw = File.join(tmp_path, "ken_all.lzh")
+      csv = File.join(tmp_path, "ken_all.csv")
+      fetch(raw)
+      decode(raw, tmp_path)
+      parse(csv)
+    end
+
     def self.fetch(path)
       open("http://www.post.japanpost.jp/zipcode/dl/kogaki/lzh/ken_all.lzh") do |f|
         File.open(path, "w") {|o| o.write(f.read)}
