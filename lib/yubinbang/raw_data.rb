d5cbde78a402b9eafea4b8db8f828597635030d3
Paul McMahon <paul@mobalean.com>
2011-01-16 13:13:13 +0900

    Parsing

--- lib/yubinbang/raw_data.rb
+++ lib/yubinbang/raw_data.rb
@@ -1,4 +1,6 @@
 require 'open-uri'
+require 'iconv'
+require 'fastercsv'
 
 module Yubinbang
   module RawData
@@ -10,6 +12,20 @@ module Yubinbang
 
     def self.decode(source, target)
       s = `cd #{target}; lha x #{source}`
+    end
+    
+    def self.parse(source)
+      iconv = Iconv.new("UTF-8", "Shift_JIS")
+      s = iconv.iconv(File.read(source))
+      a = []
+      FasterCSV.parse(s) do |row|
+        a << {
+          :postal_code => row[2],
+          :prefecture => row[6],
+          :municipality => row[7],
+        }
+      end
+      a
     end
   end
 end
--- spec/yubinbang/raw_data_spec.rb
+++ spec/yubinbang/raw_data_spec.rb
@@ -5,11 +5,26 @@ describe Yubinbang::RawData do
     raw_file = File.join(spec_root, "tmp/ken_all.lzh")
     described_class.fetch(raw_file)
     File.exist?(raw_file).should be_true
-    File.delete(raw_file)
   end
 
   it "should decode raw data" do
     raw_file = File.join(spec_root, "data/ken_all.lzh")
     described_class.decode(raw_file, File.join(spec_root, "tmp"))
+    extracted_file = File.join(spec_root, "tmp/ken_all.csv")
+    File.exist?(extracted_file).should be_true
+  end
+
+  it "should parse raw data" do
+    csv_file = File.join(spec_root, "data/ken_all.csv")
+    expected = {
+      :postal_code => "0600000",
+      :prefecture => "北海道",
+      :municipality => "札幌市中央区"
+    }
+    described_class.parse(csv_file).first.should == expected
+  end
+
+  after do
+    Dir[File.join(spec_root, "tmp/*")].each {|f| File.delete(f)}
   end
 end
